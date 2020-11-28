96b77d794934b2860cc9aa78d215f954bae6fda6
aless <alessio.tarantoo@gmail.com>
2020-11-28 16:33:34 +0100

    .

--- gestione_sito.py
+++ gestione_sito.py
@@ -1,5 +1,16 @@
+# Collegamento al sito
 import requests
-
 url = "https://one.tim.it/one/One"
+cafile = 'cacert.pem' # http://curl.haxx.se/ca/cacert.pem
+r = requests.get(url, verify = False)
 
-ret = requests.post(url, 0)
+if r == '<Response [200]>':
+    print("Collegamento riuscito")
+else:
+    print("Collegamento non riuscito")
+
+
+# Dati login
+dati_login = {"x1019428":"Cars.2020"}
+ret = requests.post(url, dati_login, verify = False)
+
