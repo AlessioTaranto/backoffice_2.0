42f2ef749c67b2e20b0d25788b255b33d3d4d71b
aless <alessio.tarantoo@gmail.com>
2020-11-29 20:34:27 +0100

    Aggiunta funzione di login

--- gestione_sito/gestione_sito.py
+++ gestione_sito/gestione_sito.py
@@ -2,19 +2,24 @@
 import time
 from selenium import webdriver
 
-
 # Preparazione apertura finestra web
 driver = webdriver.Chrome()
 url = "https://one.tim.it"
+utenza = {"x1019428":"Cars.2020"}
 
 # Apertura finestra
 driver.get(url)
 
-utenza = {"x1019428":"Cars.2020"}
 def login(username, passworld):
-    driver.find_element_by_placeholder("username").send_keys(username)
-    driver.find_element_by_placeholder("passworld").send_keys(passworld)
-    driver.find_element_by_type("submit").click()
+    print("Tentativo di login: " + url)
+    print("Nome: " + username + " Pass: " + passworld + '\n')
+    try:
+        driver.find_element_by_name("user").send_keys(username)
+        driver.find_element_by_name("pwd").send_keys(passworld)
+        driver.find_element_by_xpath("//input[@type='submit']").click()
+        print("Login riuscito")
+    except:
+        print("Login fallito")
 
 login("x1019428","Cars.2020")
 
