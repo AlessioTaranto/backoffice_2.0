c4cf0189fa70bfea9116e22903852cb37aec0802
aless <alessio.tarantoo@gmail.com>
2020-11-30 21:15:42 +0100

    Aggiunta la funzione cerca_linea Aggiunta commenti

--- gestione_sito/gestione_sito.py
+++ gestione_sito/gestione_sito.py
@@ -24,25 +24,49 @@ def login(username, passworld):
         driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pwd").send_keys(passworld)
         driver.find_element_by_xpath("//input[@type='submit']").click()
+        print("Login riuscito")
     except:
         print("ERRORE: credenziali errate")
 
-    # Accetta pagina disclaimer
+    # Accetta il disclaimer del sito
     try:
+        # Aspetta che il sito sia pronto
         WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "ao-menu-box")))
+        time.sleep(0.5)
+
+        # Accetta le condizioni del sito
         driver.find_element_by_class_name("ao-menu-box").click()
+
+    # In caso l'atttesa del sito duri troppo
     except TimeoutException:
         print("ERRORE TIMEOUT: Attesa disclaimer troppo lunga")
 
 
 # Funzione di ricerca linea
 def cerca_linea(numero_linea):
-    pass
+    print('\nRicerca linea: ' + numero_linea)
 
+    # Inserisce il numero e inizia la ricerca
+    try:
+        # Aspetta che il sito sia pronto
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'ao_line_number')))
+
+        # Inserisce il numero nella barra di ricerca
+        driver.find_element_by_id('ao_line_number').clear()
+        driver.find_element_by_id('ao_line_number').send_keys(numero_linea)
+
+        # Invia la ricerca
+        driver.find_element_by_id('ao_line_number_submit').click()
+
+        print("Ricerca riuscita")
+
+    # In caso l'atttesa del sito duri troppo
+    except TimeoutException:
+        print("ERRORE TIMEOUT: Attesa ricerca linea troppo lunga")
+
+    except:
+        print("ERRORE: errore durante la ricerca della linea")
 
 
 login("x1019428","Cars.2020")
 cerca_linea("057383193")
-
-time.sleep(5)
-
