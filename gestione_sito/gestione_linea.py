c9f293ff0f48ed2ae6fe5bce22229ccebd9f37a7
aless <alessio.tarantoo@gmail.com>
2020-12-01 21:04:03 +0100

    Creazione file

---
+++ gestione_sito/gestione_linea.py
@@ -0,0 +1,7 @@
+# Librerie
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
