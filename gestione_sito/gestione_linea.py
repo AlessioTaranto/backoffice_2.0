9add0f3b32238b9e063e7a530d002c6e82b410f4
aless <alessio.tarantoo@gmail.com>
2020-12-06 18:47:24 +0100

    aggiunta funziona di ricerca tipo di linea

--- gestione_sito/gestione_linea.py
+++ gestione_sito/gestione_linea.py
@@ -5,3 +5,22 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+
+class gestione_linea:
+    def __init__(self, driver: webdriver, tipo = 'null'):
+        self.driver = driver
+        self.tipo_linea = tipo
+
+    # Identifica tipo di linea
+    def analizza_linea(self):
+
+
+        # Se il tipo di linea non e' stato inserito lo ricerca su ONE
+        if self.tipo_linea == 'null':
+            WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, "//span[@class='ao-line-type-description']")))
+            tipo_linea_raw = self.driver.find_element_by_xpath("//span[@class='ao-line-type-description']").text
+
+            if tipo_linea_raw == 'Linea VDSL/EVDSL - FTTC TIM':
+                self.tipo_linea = 'FTTC'
+
+        print("[LINEA] tipo linea: " + self.tipo_linea)
